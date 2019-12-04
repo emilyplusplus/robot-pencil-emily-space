39ae717d3627b80c26c654072691e7699ebd9648
Emily Carlsen <emilytcyt@gmail.com>
2019-12-04 00:39:24 -0500

    Basic in memory db, and backend api, and front end fixed bg/header

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var loki = require('lokijs')
+
+let seedData = require('./seed-data.json')
+
+var db = new loki('launches.db');
+var launches = db.addCollection('launches');
+
+launches.insert(seedData)
+
 app = express();
+
+app.get('/api/all', (req, res) => {
+    res.json(launches.find())
+    //res.sendStatus(200)
+})
+
 app.use(serveStatic(__dirname + "/dist"));
+
 var port = process.env.PORT || 5000;
 app.listen(port);
+
 console.log('server started '+ port);
---
+++ src/components/Launches.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>SpaceX Launches</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Launches',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  padding: 40px;
+  margin: 0;
+}
+</style>
